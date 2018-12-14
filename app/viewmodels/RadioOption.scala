e3e12781865630d361ab42508afd9ec12515e55d
Vincent Vinnicombe <vinnycombe@igmail.com>
2018-12-14 11:03:59 +0000

    EE-62: Still wip

--- app/viewmodels/RadioOption.scala
+++ app/viewmodels/RadioOption.scala
@@ -20,8 +20,8 @@ case class RadioOption(id: String, value: String, messageKey: String)
 
 object RadioOption {
   def apply(keyPrefix: String, option: String): RadioOption = RadioOption(
-    s"$keyPrefix.$option",
-    option,
-    s"$keyPrefix.$option"
+    id = s"$keyPrefix.$option",
+    value = option,
+    messageKey = s"$keyPrefix.$option"
   )
 }
