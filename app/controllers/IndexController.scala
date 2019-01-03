32bf8953195bd3176feb8f58d2e6ddebf14c005f
Vincent Vinnicombe <vinnycombe@igmail.com>
2019-01-03 12:31:33 +0000

    EE-62: More reseting files

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -16,23 +16,18 @@
 
 package controllers
 
-import controllers.actions.{DataRetrievalAction, IdentifierAction}
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
 import views.html.IndexView
 
 class IndexController @Inject()(
                                  val controllerComponents: MessagesControllerComponents,
-                                 view: IndexView,
-                                 identify: IdentifierAction,
-                                 getData: DataRetrievalAction,
-                                 sessionRepository: SessionRepository
+                                 view: IndexView
                                ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData) { implicit request =>
+  def onPageLoad: Action[AnyContent] = Action { implicit request =>
     Ok(view())
   }
 }
