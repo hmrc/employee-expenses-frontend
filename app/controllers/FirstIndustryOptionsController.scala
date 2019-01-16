251668e9fb6ca60688311258a7548c2a47b21557
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2019-01-16 11:13:20 +0000

    EE-86: added controller and spec

--- app/controllers/FirstIndustryOptionsController.scala
+++ app/controllers/FirstIndustryOptionsController.scala
@@ -16,6 +16,65 @@
 
 package controllers
 
-class FirstIndustryOptionsController {
+import com.google.inject.Inject
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import forms.FirstIndustryOptionsFormProvider
+import models.{Enumerable, Mode, UserAnswers}
+import navigation.Navigator
+import pages.{ExpensesEmployerPaidPage, FirstIndustryOptionsPage}
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import views.html.FirstIndustryOptionsView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class FirstIndustryOptionsController @Inject()(
+                                                override val messagesApi: MessagesApi,
+                                                identify: IdentifierAction,
+                                                getData: DataRetrievalAction,
+                                                requireData: DataRequiredAction,
+                                                formProvider: FirstIndustryOptionsFormProvider,
+                                                val controllerComponents: MessagesControllerComponents,
+                                                view: FirstIndustryOptionsView,
+                                                sessionRepository: SessionRepository,
+                                                navigator: Navigator
+                                              )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport with Enumerable.Implicits {
+  val form = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+
+    implicit request =>
+
+      val preparedForm = request.userAnswers.get(FirstIndustryOptionsPage) match {
+
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+      Ok(view(preparedForm, mode))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+
+    implicit request =>
+
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest(view(formWithErrors, mode))),
+
+        value => {
+          for {
+            updatedAnswers: UserAnswers <- Future.fromTry(request.userAnswers.set(FirstIndustryOptionsPage, value))
+            _ <- sessionRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(FirstIndustryOptionsPage, mode)(updatedAnswers))
+
+
+        }
+
+      )
+
+  }
 
 }
