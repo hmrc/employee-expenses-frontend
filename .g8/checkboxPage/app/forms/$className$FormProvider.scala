508a20a343ba2fe93dfc77032399f93f5017b051
Thomas Pascoe <pascoe121@gmail.com>
2019-01-22 11:06:02 +0000

    add set to import

--- .g8/checkboxPage/app/forms/$className$FormProvider.scala
+++ .g8/checkboxPage/app/forms/$className$FormProvider.scala
@@ -4,6 +4,7 @@ import javax.inject.Inject
 
 import forms.mappings.Mappings
 import play.api.data.Form
+import play.api.data.Forms.set
 import models.$className$
 
 class $className$FormProvider @Inject() extends Mappings {
