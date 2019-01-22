b79e9bb189b5f76373e1670839790777a652559b
Thomas Pascoe <pascoe121@gmail.com>
2019-01-22 11:10:22 +0000

    add checkbox scaffold model and update options model

--- .g8/checkboxPage/app/models/$className$.scala
+++ .g8/checkboxPage/app/models/$className$.scala
@@ -1,7 +1,6 @@
 package models
 
-import play.api.libs.json._
-import viewmodels.RadioOption
+import viewmodels.RadioCheckboxOption
 
 sealed trait $className$
 
@@ -9,9 +8,10 @@ object $className$ extends Enumerable.Implicits {
 
   case object $option1key;format="Camel"$ extends WithName("$option1key;format="decap"$") with $className$
   case object $option2key;format="Camel"$ extends WithName("$option2key;format="decap"$") with $className$
+  case object $option3key;format="Camel"$ extends WithName("$option3key;format="decap"$") with $className$
 
   val values: Seq[$className$] = Seq(
-    $option1key;format="Camel"$, $option2key;format="Camel"$
+    $option1key;format="Camel"$, $option2key;format="Camel"$, $option3key;format="Camel"$
   )
 
   val options: Seq[RadioCheckboxOption] = values.map {
