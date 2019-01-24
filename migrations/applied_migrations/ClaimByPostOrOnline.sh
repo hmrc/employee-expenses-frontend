490688f8b558ae553f53ec987da7180b9a0c1953
Matthew Beck <MatthewAlexanderBeck@users.noreply.github.com>
2019-01-24 13:47:16 +0000

    Ee 133 (#52)
    * EE-133: Initial page creation
    
    * EE-133: Moved to police folder
    
    * EE-133: Updated content and added url link
    
    * EE-133: updated pageWithButtonLink behaviour
    
    * EE-133: added missing page title

---
+++ migrations/applied_migrations/ClaimByPostOrOnline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration ClaimByPostOrOnline"
+
+echo "Adding routes to conf/app.routes"
+echo "" >> ../conf/app.routes
+echo "GET        /claimByPostOrOnline                       controllers.police.ClaimByPostOrOnlineController.onPageLoad()" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "claimByPostOrOnline.title = claimByPostOrOnline" >> ../conf/messages.en
+echo "claimByPostOrOnline.heading = claimByPostOrOnline" >> ../conf/messages.en
+
+echo "Migration ClaimByPostOrOnline completed"
