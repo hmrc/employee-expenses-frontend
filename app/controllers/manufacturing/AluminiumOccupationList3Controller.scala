3ed9ea2be42b9cb51e8d535ce60924d324c0c457
garethgooch <31101509+garethgooch@users.noreply.github.com>
2019-01-29 11:58:29 +0000

    EE-219: fix list3 idenify

--- app/controllers/manufacturing/AluminiumOccupationList3Controller.scala
+++ app/controllers/manufacturing/AluminiumOccupationList3Controller.scala
@@ -35,7 +35,7 @@ class AluminiumOccupationList3Controller @Inject()(
                                          override val messagesApi: MessagesApi,
                                          sessionRepository: SessionRepository,
                                          @Named("Manufacturing") navigator: Navigator,
-                                         identify: IdentifierAction,
+                                         identify: UnauthenticatedIdentifierAction,
                                          getData: DataRetrievalAction,
                                          requireData: DataRequiredAction,
                                          formProvider: AluminiumOccupationList3FormProvider,
