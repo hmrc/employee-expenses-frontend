4dc53aa64f71d0573dc3efe9652bf04d20afc574
Thomas Pascoe <15924950+Pascoe121@users.noreply.github.com>
2019-04-02 12:04:50 +0100

    EE-436 add service name and GOV.UK to every page title

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -25,6 +25,8 @@ import play.api.mvc.Call
 @Singleton
 class FrontendAppConfig @Inject() (configuration: Configuration) {
 
+  lazy val serviceTitle = "Employee Expenses - GOV.UK"
+
   private val contactHost = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "employeeExpensesFrontend"
 
