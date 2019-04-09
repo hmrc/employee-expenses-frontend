b449b801cc8a454436935c9fb9897f544eea2b80
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2019-04-09 11:47:40 +0100

    Ee 433 (#269)
    * EE-433: Added form partial retriever, updated govuk template version and removed bundled gtm scripts as included in new govuk template, wip
    
    * EE-433: Created MockEeFormPartialRetriever to inject into applicationBuilder for testing purposes, tests running and passing
    
    * EE-433: Removed duplicated copyright notice

---
+++ test/utils/MockEeFormPartialRetriever.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import com.google.inject.Inject
+import config.EeFormPartialRetriever
+import org.scalatest.mockito.MockitoSugar
+import play.api.mvc.RequestHeader
+import play.twirl.api.Html
+import uk.gov.hmrc.play.bootstrap.filters.frontend.crypto.SessionCookieCrypto
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+
+class MockEeFormPartialRetriever @Inject()(httpGet: HttpClient, sessionCookieCrypto: SessionCookieCrypto)
+  extends EeFormPartialRetriever(httpGet, sessionCookieCrypto) with MockitoSugar {
+
+  override def getPartialContent(url: String, templateParameters: Map[String, String], errorMessage: Html)(implicit request: RequestHeader): Html = {
+    Html("")
+  }
+}
