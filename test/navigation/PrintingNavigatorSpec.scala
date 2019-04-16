cbdac0b34b6add728574fdb188098d90daf4da38
Thomas Pascoe <15924950+Pascoe121@users.noreply.github.com>
2019-04-16 14:55:44 +0100

    add session expired tests

--- test/navigation/PrintingNavigatorSpec.scala
+++ test/navigation/PrintingNavigatorSpec.scala
@@ -19,15 +19,24 @@ package navigation
 import base.SpecBase
 import models.{CheckMode, NormalMode}
 import controllers.printing.routes
+import controllers.routes.SessionExpiredController
+import org.scalatest.mockito.MockitoSugar
+import pages.Page
 import pages.printing._
 
-class PrintingNavigatorSpec extends SpecBase {
+class PrintingNavigatorSpec extends SpecBase with MockitoSugar {
   private val navigator = new PrintingNavigator
   private val modes = Seq(NormalMode, CheckMode)
 
   "PrintingNavigator" when {
     for (mode <- modes) {
       s"in $mode" must {
+
+        "go to SessionExpired when not a printing page" in {
+          navigator.nextPage(mock[Page], mode)(emptyUserAnswers) mustBe
+            SessionExpiredController.onPageLoad()
+        }
+
         "go to EmployerContributionController from PrintingOccupationList1Page when 'Yes' is selected" in {
           val answers = emptyUserAnswers.set(PrintingOccupationList1Page, true).success.value
           navigator.nextPage(PrintingOccupationList1Page, mode)(answers) mustBe
@@ -38,6 +47,11 @@ class PrintingNavigatorSpec extends SpecBase {
           val answers = emptyUserAnswers.set(PrintingOccupationList1Page, false).success.value
           navigator.nextPage(PrintingOccupationList1Page, mode)(answers) mustBe
             routes.PrintingOccupationList2Controller.onPageLoad(mode)
+        }
+
+        "go to SessionExpiredController from PrintingOccupationList1Page when None" in {
+          navigator.nextPage(PrintingOccupationList1Page, mode)(emptyUserAnswers) mustBe
+            controllers.routes.SessionExpiredController.onPageLoad()
         }
 
         "go to EmployerContributionController from PrintingOccupationList2Controller when 'Yes' is selected" in {
--- test/navigation/ShipyardNavigatorSpec.scala
+++ test/navigation/ShipyardNavigatorSpec.scala
@@ -18,11 +18,14 @@ package navigation
 
 
 import base.SpecBase
+import controllers.routes.SessionExpiredController
 import models._
+import org.scalatest.mockito.MockitoSugar
+import pages.Page
 import pages.shipyard._
 
 
-class ShipyardNavigatorSpec extends SpecBase {
+class ShipyardNavigatorSpec extends SpecBase with MockitoSugar {
   private val modes = Seq(NormalMode, CheckMode)
   private val navigator = new ShipyardNavigator
 
@@ -30,6 +33,11 @@ class ShipyardNavigatorSpec extends SpecBase {
 
     for (mode <- modes) {
       s"in $mode" must {
+
+        "go to SessionExpired when not a shipyard page" in {
+          navigator.nextPage(mock[Page], mode)(emptyUserAnswers) mustBe
+            SessionExpiredController.onPageLoad()
+        }
 
         "go to EmployerContributionPage when Appprentice 'yes' is selected" in {
           val answers = emptyUserAnswers.set(ShipyardApprenticeStorekeeperPage, true).success.value
@@ -43,6 +51,11 @@ class ShipyardNavigatorSpec extends SpecBase {
 
           navigator.nextPage(ShipyardApprenticeStorekeeperPage, mode)(answers) mustBe
             controllers.shipyard.routes.ShipyardOccupationList1Controller.onPageLoad(mode)
+        }
+
+        "go to SessionExpired when Appprentice None" in {
+          navigator.nextPage(ShipyardApprenticeStorekeeperPage, mode)(emptyUserAnswers) mustBe
+            controllers.routes.SessionExpiredController.onPageLoad()
         }
 
         "go to EmployerContributionPage when ShipyardOccupationList1 'Yes selected" in {
@@ -59,6 +72,11 @@ class ShipyardNavigatorSpec extends SpecBase {
             controllers.shipyard.routes.ShipyardOccupationList2Controller.onPageLoad(mode)
         }
 
+        "go to SessionExpired when ShipyardOccupationList1 None" in {
+          navigator.nextPage(ShipyardOccupationList1Page, mode)(emptyUserAnswers) mustBe
+            controllers.routes.SessionExpiredController.onPageLoad()
+        }
+
         "go to EmployerContributionPage when ShipyardOccupationList2 'Yes selected" in {
           val answers = emptyUserAnswers.set(ShipyardOccupationList2Page, true).success.value
 
@@ -71,6 +89,11 @@ class ShipyardNavigatorSpec extends SpecBase {
 
           navigator.nextPage(ShipyardOccupationList2Page, mode)(answers) mustBe
             controllers.shipyard.routes.LabourerController.onPageLoad(mode)
+        }
+
+        "go to SessionExpired when ShipyardOccupationList2 None" in {
+          navigator.nextPage(ShipyardOccupationList2Page, mode)(emptyUserAnswers) mustBe
+            controllers.routes.SessionExpiredController.onPageLoad()
         }
 
         "go to EmployerContributionPage when Labourer 'Yes selected" in {
