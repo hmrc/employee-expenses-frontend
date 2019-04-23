3d9a4eb6ae58806bacf3572e91b2ac4cb23a4fa4
Vincent Vinnicombe <->
2019-04-23 08:15:13 +0100

    library-upgrades: Library uprgades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,20 +8,20 @@ object AppDependencies {
     "org.reactivemongo"     %% "play2-reactivemongo"            % "0.16.0-play26",
     "uk.gov.hmrc"           %% "logback-json-logger"            % "4.4.0",
     "uk.gov.hmrc"           %% "govuk-template"                 % "5.33.0-play-26",
-    "uk.gov.hmrc"           %% "play-health"                    % "3.12.0-play-26",
-    "uk.gov.hmrc"           %% "play-ui"                        % "7.33.0-play-26",
+    "uk.gov.hmrc"           %% "play-health"                    % "3.14.0-play-26",
+    "uk.gov.hmrc"           %% "play-ui"                        % "7.39.0-play-26",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping"  % "0.2.0",
     "uk.gov.hmrc"           %% "bootstrap-play-26"              % "0.37.0",
     "uk.gov.hmrc"           %% "play-whitelist-filter"          % "2.0.0",
     "uk.gov.hmrc"           %% "tax-year"                       % "0.4.0",
-    "uk.gov.hmrc"           %% "play-partials"                  % "6.7.0-play-26",
+    "uk.gov.hmrc"           %% "play-partials"                  % "6.9.0-play-26",
     "com.typesafe.play"     %% "play-json-joda"                 % "2.7.1",
     "org.scalatra.scalate"  %% "play-scalate"                   % "0.5.0",
     "org.scalatra.scalate"  %% "scalate-core"                   % "1.9.1"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "hmrctest"             % "3.5.0-play-26",
+    "uk.gov.hmrc"             %% "hmrctest"             % "3.8.0-play-26",
     "org.scalatest"           %% "scalatest"            % "3.0.4",
     "org.scalatestplus.play"  %% "scalatestplus-play"   % "2.0.1",
     "org.pegdown"             %  "pegdown"              % "1.6.0",
