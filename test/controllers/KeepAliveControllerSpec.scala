a4dc1763d96b2fe970afc2500e0df6489fd81ab4
Matthew Beck <dougledog@live.co.uk>
2019-09-02 07:49:57 +0100

    EE-1022: Added specs

---
+++ test/controllers/KeepAliveControllerSpec.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.time.LocalDateTime
+
+import base.SpecBase
+import controllers.actions.Authed
+import models.UserAnswers
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.mockito.MockitoSugar
+import play.api.inject.bind
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+
+import scala.concurrent.Future
+
+class KeepAliveControllerSpec extends SpecBase with MockitoSugar with ScalaFutures with IntegrationPatience with BeforeAndAfterEach {
+
+  private val mockSessionRepository: SessionRepository = mock[SessionRepository]
+
+  override def beforeEach(): Unit = {
+    reset(mockSessionRepository)
+  }
+
+  "KeepAlive Controller" must {
+
+    "return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[SessionRepository].toInstance(mockSessionRepository))
+        .build()
+
+      when(mockSessionRepository.updateTimeToLive(any())).thenReturn(Future.successful(true))
+
+      val request = FakeRequest(GET, routes.KeepAliveController.keepAlive().url)
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      whenReady(result) {
+        _ =>
+          verify(mockSessionRepository, times(1)).updateTimeToLive(Authed(userAnswersId))
+      }
+
+      application.stop()
+    }
+
+    "Redirect to Session Expired when updateTimeToLive fails" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[SessionRepository].toInstance(mockSessionRepository))
+        .build()
+
+      when(mockSessionRepository.updateTimeToLive(any())).thenReturn(Future.failed(new Exception))
+
+      val request = FakeRequest(GET, routes.KeepAliveController.keepAlive().url)
+
+      val result = route(application, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual routes.SessionExpiredController.onPageLoad().url
+
+      application.stop()
+    }
+  }
+
+}
