4fad66f90119a2465ff58a98e49e3f3b33bb6ba0
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-09-16 11:52:59 +0100

    WIP Submit controller.

---
+++ app/controllers/authenticated/SubmissionController.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.authenticated
+
+import controllers.actions._
+import controllers.routes.{PhoneUsController, TechnicalDifficultiesController}
+import javax.inject.Inject
+import models.{NormalMode, TaxYearSelection, UserAnswers}
+import models.TaxYearSelection.CurrentYearMinus1
+import models.auditing.AuditData
+import models.auditing.AuditEventType.{UpdateFlatRateExpenseFailure, UpdateFlatRateExpenseSuccess}
+import pages.ClaimAmountAndAnyDeductions
+import pages.authenticated.{ChangeWhichTaxYearsPage, CheckYourAnswersPage, RemoveFRECodePage, TaxYearSelectionPage}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import service.SubmissionService
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+
+import scala.concurrent.ExecutionContext
+
+class SubmissionController @Inject()(override val messagesApi: MessagesApi,
+                                     identify: AuthenticatedIdentifierAction,
+                                     getData: DataRetrievalAction,
+                                     requireData: DataRequiredAction,
+                                     submissionService: SubmissionService,
+                                     auditConnector: AuditConnector,
+                                     val controllerComponents: MessagesControllerComponents
+                                   )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData).async {
+    implicit request =>
+      val dataToAudit: AuditData =
+        AuditData(nino = request.nino.get, userAnswers = request.userAnswers.data)
+
+      (
+        request.userAnswers.get(TaxYearSelectionPage),
+        request.userAnswers.get(ClaimAmountAndAnyDeductions),
+        request.userAnswers.get(RemoveFRECodePage),
+        request.userAnswers.get(ChangeWhichTaxYearsPage)
+      ) match {
+        case (Some(taxYears), Some(_), Some(removeYear), None) =>
+          submissionService.removeFRE(request.nino.get, taxYears, removeYear).map(
+            result =>
+              auditAndRedirect(result, dataToAudit, request.userAnswers)
+          )
+      }
+  }
+
+  private def auditAndRedirect(result: Seq[HttpResponse],
+                               auditData: AuditData,
+                               userAnswers: UserAnswers
+                              )(implicit hc: HeaderCarrier): Result = {
+
+    if (result.nonEmpty && result.forall(_.status == 204)) {
+      auditConnector.sendExplicitAudit(UpdateFlatRateExpenseSuccess.toString, auditData)
+      Redirect(navigator.nextPage(CheckYourAnswersPage, NormalMode)(userAnswers))
+    } else if (result.nonEmpty && result.exists(_.status == 423)) {
+      Redirect(PhoneUsController.onPageLoad())
+    } else {
+      auditConnector.sendExplicitAudit(UpdateFlatRateExpenseFailure.toString, auditData)
+      Redirect(TechnicalDifficultiesController.onPageLoad())
+    }
+  }
+
+}
