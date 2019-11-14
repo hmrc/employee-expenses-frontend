0af474c16adfcd615dfcc3dc23c13f51390bfd18
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2019-11-14 16:12:51 +0000

    DL-2474: Add Welsh messages and MessagesSpec

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "org.reactivemongo"     %% "play2-reactivemongo"            % "0.16.0-play26",
     "uk.gov.hmrc"           %% "logback-json-logger"            % "4.6.0",
-    "uk.gov.hmrc"           %% "govuk-template"                 % "5.43.0-play-26",
+    "uk.gov.hmrc"           %% "govuk-template"                 % "5.44.0-play-26",
     "uk.gov.hmrc"           %% "play-health"                    % "3.14.0-play-26",
     "uk.gov.hmrc"           %% "play-ui"                        % "8.3.0-play-26",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping"  % "0.2.0",
@@ -22,13 +22,13 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"           %% "scalatest"            % "3.0.8",
-    "org.scalatestplus.play"  %% "scalatestplus-play"   % "2.0.1",
+    "org.scalatestplus.play"  %% "scalatestplus-play"   % "3.1.2",
     "org.pegdown"             %  "pegdown"              % "1.6.0",
-    "org.jsoup"               %  "jsoup"                % "1.10.3",
+    "org.jsoup"               %  "jsoup"                % "1.12.1",
     "com.typesafe.play"       %% "play-test"            % PlayVersion.current,
     "org.mockito"             %  "mockito-all"          % "1.10.19",
     "org.scalacheck"          %% "scalacheck"           % "1.14.2",
-    "com.github.tomakehurst"  %  "wiremock-standalone"  % "2.19.0"
+    "com.github.tomakehurst"  %  "wiremock-standalone"  % "2.25.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
