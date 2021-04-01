901432792e90c9b546ba87d284677d4ea7733589
grahampaulcook <grahampaulcook@users.noreply.github.com>
2021-04-01 15:30:55 +0100

    DL-5220 Play 2.7 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,16 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"           %% "simple-reactivemongo"           % "7.31.0-play-26",
+    "uk.gov.hmrc"           %% "simple-reactivemongo"           % "7.31.0-play-27",
     "uk.gov.hmrc"           %% "logback-json-logger"            % "5.1.0",
-    "uk.gov.hmrc"           %% "govuk-template"                 % "5.65.0-play-26",
-    "uk.gov.hmrc"           %% "play-health"                    % "3.16.0-play-26",
-    "uk.gov.hmrc"           %% "play-ui"                        % "9.1.0-play-26",
-    "uk.gov.hmrc"           %% "http-caching-client"            % "9.2.0-play-26",
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping"  % "1.6.0-play-26",
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-26"     % "3.4.0",
+    "uk.gov.hmrc"           %% "govuk-template"                 % "5.65.0-play-27",
+    "uk.gov.hmrc"           %% "play-health"                    % "3.16.0-play-27",
+    "uk.gov.hmrc"           %% "play-ui"                        % "9.1.0-play-27",
+    "uk.gov.hmrc"           %% "http-caching-client"            % "9.2.0-play-27",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping"  % "1.6.0-play-27",
+    "uk.gov.hmrc"           %% "bootstrap-frontend-play-27"     % "3.4.0",
     "uk.gov.hmrc"           %% "tax-year"                       % "1.1.0",
-    "uk.gov.hmrc"           %% "play-partials"                  % "7.1.0-play-26",
+    "uk.gov.hmrc"           %% "play-partials"                  % "7.1.0-play-27",
     "uk.gov.hmrc"           %% "local-template-renderer"        % "2.10.0-play-26",
     "com.typesafe.play"     %% "play-json-joda"                 % "2.9.0",
     "org.scalatra.scalate"  %% "play-scalate"                   % "0.6.0",
@@ -23,7 +23,7 @@ object AppDependencies {
 
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatestplus.play"  %% "scalatestplus-play"   % "3.1.3",
+    "org.scalatestplus.play"  %% "scalatestplus-play"   % "4.0.3",
     "org.pegdown"             %  "pegdown"              % "1.6.0",
     "org.jsoup"               %  "jsoup"                % "1.13.1",
     "com.typesafe.play"       %% "play-test"            % PlayVersion.current,
