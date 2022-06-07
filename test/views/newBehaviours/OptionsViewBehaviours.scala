81ea152187cbf166881e8d5885da2340ec371acd
selinahussain <61423748+selinahussain@users.noreply.github.com>
2022-06-07 13:02:56 +0100

    DL-7720: Construction view and tests updated, error handling updated

--- test/views/newBehaviours/OptionsViewBehaviours.scala
+++ test/views/newBehaviours/OptionsViewBehaviours.scala
@@ -24,7 +24,8 @@ trait OptionsViewBehaviours[A] extends ViewBehaviours {
 
   def optionsPage(form: Form[A],
                   createView: Form[A] => HtmlFormat.Appendable,
-                  options: Seq[RadioCheckboxOption]): Unit = {
+                  options: Seq[RadioCheckboxOption],
+                  radioOr: Boolean = false): Unit = {
 
     "behave like an options page" must {
 
@@ -34,11 +35,23 @@ trait OptionsViewBehaviours[A] extends ViewBehaviours {
 
         for (option <- options) {
 
-          assertContainsRadioButton(doc, option.id, "value", option.value, false)
+          val idVal = if(radioOr && option == options.last){
+            s"value-${options.indexOf(option) + 2}"
+          } else if(option != options.head){
+            s"value-${options.indexOf(option) + 1}"
+          } else {s"value"}
+
+          assertContainsRadioButton(doc, idVal, "value", option.value, false)
         }
       }
     }
     for (option <- options) {
+
+      val idVal = if(radioOr && option == options.last){
+        s"value-${options.indexOf(option) + 2}"
+      } else if(option != options.head){
+        s"value-${options.indexOf(option) + 1}"
+      } else {s"value"}
 
       s"rendered with a value of '${option.value}' " must {
 
@@ -46,11 +59,17 @@ trait OptionsViewBehaviours[A] extends ViewBehaviours {
 
           val doc = asDocument(createView(form.bind(Map("value" -> s"${option.value}"))))
 
-          assertContainsRadioButton(doc, option.id, "value", option.value, true)
+          assertContainsRadioButton(doc, idVal, "value", option.value, true)
 
           for (unselectedOption <- options.filterNot(o => o == option)) {
 
-            assertContainsRadioButton(doc, unselectedOption.id, "value", unselectedOption.value, false)
+            val unselectId = if(radioOr && unselectedOption == options.last){
+              s"value-${options.indexOf(unselectedOption) + 2}"
+            } else if(unselectedOption != options.head) {
+              s"value-${options.indexOf(unselectedOption) + 1}"
+            } else{s"value"}
+
+            assertContainsRadioButton(doc, unselectId, "value", unselectedOption.value, false)
 
           }
         }
