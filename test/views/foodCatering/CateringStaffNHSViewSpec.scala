530a81622ef899b7040f2b7060a463a863c2994f
selinahussain <61423748+selinahussain@users.noreply.github.com>
2022-06-14 14:44:39 +0100

    DL-7724: cateringStaffNhs view updated

--- test/views/foodCatering/CateringStaffNHSViewSpec.scala
+++ test/views/foodCatering/CateringStaffNHSViewSpec.scala
@@ -21,7 +21,7 @@ import forms.foodCatering.CateringStaffNHSFormProvider
 import models.NormalMode
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
-import views.behaviours.YesNoViewBehaviours
+import views.newBehaviours.YesNoViewBehaviours
 import views.html.foodCatering.CateringStaffNHSView
 
 class CateringStaffNHSViewSpec extends YesNoViewBehaviours {
