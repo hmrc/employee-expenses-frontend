34d105239c17083924ba07ccfa8b1062d02422d8
geethumathew <32937184+geethumathew@users.noreply.github.com>
2024-11-12 18:42:57 +0000

    DL-14812:Removed content and URL link around 'online form'

--- test/views/ClaimByAlternativeViewSpec.scala
+++ test/views/ClaimByAlternativeViewSpec.scala
@@ -35,7 +35,7 @@ class ClaimByAlternativeViewSpec extends ViewBehaviours {
 
     behave like pageWithBackLink(applyView)
 
-    val link2: Html = Html(s"""<a class="govuk-link" href="${frontendAppConfig.p87ClaimByPostUrl}">${messages("claimByAlternative.claimByPostLink")}</a>""")
+    val link2: Html = Html(s"""<a class="govuk-link" href="${frontendAppConfig.p87ClaimByPostUrl}">${messages("claimByAlternative.claimByPostLinkText")}</a>""")
 
     behave like pageWithBodyText(
       applyView,
