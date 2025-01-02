f0fa2140b426fa493743851277e58e95e496768c
berkb41 <176663520+berkb41@users.noreply.github.com>
2025-01-02 10:27:21 +0000

    DL-15072:Updating service with latest version of dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.3.0"
-  private val bootstrapVersion = "9.6.0"
+  private val mongoVersion = "2.2.0"
+  private val bootstrapVersion = "9.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
